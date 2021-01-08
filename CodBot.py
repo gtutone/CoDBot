524c536619d6b93986e57413dd99cbacfadb7e47
gtutone <67984002+gtutone@users.noreply.github.com>
2021-01-08 08:53:19 -0800

    Bot is working!

--- CodBot.py
+++ CodBot.py
@@ -7,6 +7,8 @@ Created on Wed Jan  6 22:17:54 2021
 
 import discord
 from discord.ext import commands
+from Cards import CardDict
+import re
 
 client = commands.Bot(command_prefix='!')
 PlayedGame = 'Card of Darkness'
@@ -16,8 +18,17 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(PlayedGame))
 
-# @client.command()
-# async def
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'Card of' or 'card of' in message.content:
+        # Find the card in the message and make every first letter uppercase
+        CardExtract = str(re.findall('card of \w+', message.content, re.IGNORECASE)).title()[2:-2]
+        CardLookup = CardDict[CardExtract]
+        Description = str(CardExtract + ': ' + CardLookup)
+        await message.channel.send(Description)
 
 with open("token.txt", "r", encoding="utf-8") as token:
     botToken = token.read()
