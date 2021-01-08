ed97f8eef265344be7323d3abfc303daafa133af
gtutone <67984002+gtutone@users.noreply.github.com>
2021-01-07 23:38:53 -0800

    Updating to hide token

--- CodBot.py
+++ CodBot.py
@@ -5,17 +5,6 @@ Created on Wed Jan  6 22:17:54 2021
 @author: gabrieltutone
 """
 
-# client = commands.Bot(command_prefix='!')
-#
-# @client.event
-# async def on_ready():
-#     print('Bot is ready')
-#
-# @client.event
-# async def on_member_join(member):
-#
-# client.run('Nzk2NjE5NjIxNTM1OTczMzc2.X_aj1g.vOKGxNl5sM8eUniJf-cPczhduS8')
-
 import discord
 from discord.ext import commands
 
@@ -27,7 +16,10 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(PlayedGame))
 
-@client.command()
-async def
+# @client.command()
+# async def
 
-client.run('Nzk2NjE5NjIxNTM1OTczMzc2.X_aj1g.vOKGxNl5sM8eUniJf-cPczhduS8')
+with open("token.txt", "r", encoding="utf-8") as token:
+    botToken = token.read()
+
+client.run(botToken)
