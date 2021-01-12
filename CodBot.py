e9cf85fbd768756e8c9ec0d9d47fe6f66253ecd6
gtutone <67984002+gtutone@users.noreply.github.com>
2021-01-11 23:40:06 -0800

    Trying to implement fuzzy matches. Still some bugs.

--- CodBot.py
+++ CodBot.py
@@ -8,28 +8,40 @@ Created on Wed Jan  6 22:17:54 2021
 import discord
 import re
 import csv
+from fuzzywuzzy import process
 
 def CardSearch(card):
     CardDesc = ''
+    CardTitles = []
     # Read the csv file containing the cards
     with open('CardsList.csv', newline='') as csvfile:
         CardReader = csv.DictReader(csvfile, delimiter=',')
 
-        # Iterate over all the cards and check if the title matches
+        # Find card with highest fuzzy match
         for row in CardReader:
-            if card == row['Title']:
-                # If card has no tier modifiers
-                if (row['Level 1'] or row['Level 1'] or row['Level 1']) == 'n/a':
-                    CardDesc = str(row['Title']) +'\n'+ str(row['Description']) +'\n'+ 'No tier modifiers'
-                    return CardDesc
+            CardTitles.append(row['Title'])
+        FuzzyMatch = process.extractOne(card, CardTitles)
+        FuzzyRatio = FuzzyMatch[1]
+        CardMatch = str(FuzzyMatch)[2:-6]
+        if FuzzyRatio != 100:
+            print('Did you mean: ' + CardMatch + '?')
 
-                # If card has tier modifiers, append them to output
-                else:
-                    CardDesc = str(row['Title']) +'\n'+ str(row['Description']) +'\n'+\
-                               'Tier 1: ' + str(row['Level 1']) + ', '+\
-                               'Tier 2: ' + str(row['Level 2']) + ', '+\
-                               'Tier 3: ' + str(row['Level 3'])
-                    return CardDesc
+        else:
+            # Iterate over all the cards and check if the title matches
+            for row in CardReader:
+                if CardMatch == row['Title']:
+                    # If card has no tier modifiers
+                    if (row['Level 1'] or row['Level 1'] or row['Level 1']) == 'n/a':
+                        CardDesc = str(row['Title']) +'\n'+ str(row['Description']) +'\n'+ 'No tier modifiers'
+                        return CardDesc
+
+                    # If card has tier modifiers, append them to output
+                    else:
+                        CardDesc = str(row['Title']) +'\n'+ str(row['Description']) +'\n'+\
+                                   'Tier 1: ' + str(row['Level 1']) + ', '+\
+                                   'Tier 2: ' + str(row['Level 2']) + ', '+\
+                                   'Tier 3: ' + str(row['Level 3'])
+                        return CardDesc
 
     # If no card found, let user know
     if CardDesc == '':
@@ -66,7 +78,7 @@ async def on_message(message):
     # Look for messages that contain 'card'
     if 'card' in messageLow:
         # Find the card in the message and make every first letter uppercase
-        CardExtract = str(re.findall('the card of \w+', message.content, re.IGNORECASE)).title()[2:-2]
+        CardExtract = str(re.findall('card of \w+', message.content, re.IGNORECASE)).title()[2:-2]
         # Lookup card using the CardSearch function
         CardLookup = CardSearch(CardExtract)
         # Send card description text to channel
